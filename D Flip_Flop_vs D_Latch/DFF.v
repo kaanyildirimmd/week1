47ea0335533465e12bc23ba4e494a2f59067c7b2
kaanyildirimmd <111732430+kaanyildirimmd@users.noreply.github.com>
2023-09-23 13:58:44 +0300

    Add files via upload

---
+++ D Flip_Flop_vs D_Latch/DFF.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/22/2023 02:47:22 PM
+// Design Name: 
+// Module Name: DFF
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module d_flip_flops_16 (
+    input  clk,        
+    input resetn,      
+    input [1:0] byteena,   
+    input [15:0] d,        
+    output reg [15:0] q         
+);
+
+  
+
+   always @(posedge clk) begin
+      
+     if (!resetn) begin
+        q <=16'b0;
+    end 
+    else begin
+    if (byteena[0]) begin
+      q[7:0] <= d[7:0];
+    end
+    if (byteena[1]) begin
+      q[15:8] <= q[15:8];
+    end
+    end
+    end
+    
+endmodule
